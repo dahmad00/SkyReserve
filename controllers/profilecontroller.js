eab61678b17dd6da6d768336a2440a978c511bcd
Hurerah <f200259@cfd.nu.edu.pk>
2023-03-08 02:26:15 +0500

    sdasdasdsd asdas sdasd asdag 3312

---
+++ controllers/profilecontroller.js
@@ -0,0 +1,16 @@
+exports.Home=function(req,res)
+{
+    res.send("HOME PAGE");
+};
+exports.Account=function(req,res)
+{
+    res.send("Account Page");
+};
+exports.About=function(req,res)
+{
+    res.send("About Page");
+};
+exports.Friends=function(req,res)
+{
+    res.send("Friends Page");
+};
