b1a34c75c60e47baf24fcbf8f95d025d40a7cd9e
= <=>
2023-03-09 14:57:43 +0500

    User.js, Index.js updated for sequelize table

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const express = require('express')
 const app = express()
+var mysql = require('mysql2/promise')
+
 
 const router=require("./indexroutes")
+
+
+const userModel = require('./models/user')
+
+
 app.use('/',router)
+
+
+userModel.createUser();
+
+var con
+
+
+// async function queryFunction() {
+//     var connection = await mysql.createConnection({
+//         host     : 'localhost',
+//         user     : 'root',
+//         password : '',
+//         database : 'books'
+//       });
+//     [con] = await connection.execute(
+//         'SELECT * FROM book'
+//     );    
+
+//     console.log(con)
+// }
+
+// queryFunction();
+
+
+
+
 
 app.listen(3000)
