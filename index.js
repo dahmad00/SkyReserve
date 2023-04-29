6361c8f7dc9f96cb1ac3893e61292faedbef2d8d
Aish <f200360@cfd.nu.edu.pk>
2023-04-29 23:20:31 +0500

    node multer

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const app = express();
+const path=require('path');
+const multer  = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'Images')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + path.extname(file.originalname)
+    console.log(file)
+    cb(null,  uniqueSuffix)
+  }
+})
+const uploads = multer({ storage: storage })
+app.set('view engine', 'ejs');
+
+app.get('/upload', function(req, res) {
+  res.render('upload');
+});
+
+app.post('/upload', uploads.single('image'),function(req, res) {
+  res.send('image uploaded');
+});
 
 const {sequelize,DataTypes}=require('./connection')
 const errorMiddleWare = require('./middleware/error')
 
 //const cors = require("cors");
-const app = express();
+
 
 const customerRoutes = require('./routes/customer.routes');
 const seatRoutes = require('./routes/seat.routes');
@@ -96,14 +120,42 @@ app.listen(PORT, () => {
   console.log(`Server is running on port http://127.0.01:${PORT} .`);
 });
 
+/*
 
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(req.file, req.body)
+})
 
+app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+})
 
-// customer.hasMany(booking,{foreignKey:'BookingID',as:'Booking'});
-// customer.hasMany(payment,{foreignKey:'paymentID',as:'payment'});
-// booking.belongsTo(seat,{foreignKey:'seatID'});
-// airport.hasMany(flight,{foreignKey:'flightID',as:'flightreserving'});
-// flight.hasMany(seat);
+const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+app.post('/cool-profile', cpUpload, function (req, res, next) {
+  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+  //
+  // e.g.
+  //  req.files['avatar'][0] -> File
+  //  req.files['gallery'] -> Array
+  //
+  // req.body will contain the text fields, if there were any
+})
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
 
+//const upload = multer({ storage: storage })
+//const storage = multer.memoryStorage()
+//const upload = multer({ storage: storage })
 
+*/
 app.listen(3000);
